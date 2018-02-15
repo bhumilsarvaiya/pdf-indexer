470971dcfe5334959fdce7aed4ec6b3d7943637b
bhumilsarvaiya <bhumilsarvaiya@gmail.com>
2018-02-15 18:27:35 +0530

    no need of database

--- server/routes/deleteFile.js
+++ server/routes/deleteFile.js
@@ -1,9 +1,6 @@
 const express = require('express');
 let router = express.Router();
 var app = express();
-const r = require('../db/rethinkdb');
-const DB_NAME = process.env.db || 'PDF_Extract'; //DB used to store uploaded file names
-const TABLE_NAME = process.env.table || 'pdflist'; //Table used to store uploaded file names
 var fs = require('fs');
 var bodyParser = require('body-parser')
 
@@ -14,7 +11,6 @@ router.get('/', async (req, res) => {
     let mainFilePath = process.cwd();
     let dir = mainFilePath +'/public/'+ req.query.filename;
     await fs.unlinkSync(dir);
-    let result = await r.db(DB_NAME).table(TABLE_NAME).get(req.query.id).delete().run();
     res.send(result);
   } catch (err) {
     // debug('Error:', err);
--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,13 +1,10 @@
 const express = require('express');
 let router = express.Router();
-const r = require('../db/rethinkdb');
 const request = require('request-promise');
-const DB_NAME = process.env.db || 'PDF_Extract'; //DB used to store uploaded file names
-const TABLE_NAME = process.env.table || 'pdflist'; //Table used to store uploaded file names
 const debug = require('debug')('/upload');
 const _ = require('lodash');
 let getFileList = require('../helpers/getFileList')
-var fs = require('fs');
+const fs = require('fs');
 
 router.post('/', async (req, res) => {
   if (!req.files)
@@ -23,14 +20,13 @@ router.post('/', async (req, res) => {
     if (_.findIndex(fileList, {fileName: sampleFile.name}) !== -1) {
       res.status(400).send(sampleFile.name + ' aleady exists');
     } else {
-      
+
       let mainFilePath = process.cwd();
       let dir = mainFilePath +'/public';
-      if (!fs.existsSync(dir)){
+      if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
-    }
+      }
       await sampleFile.mv(mainFilePath + '/public/' + sampleFile.name)
-      await r.db(DB_NAME).table(TABLE_NAME).insert({'fileName': sampleFile.name, 'created_at': new Date().toJSON()}).run()
       res.send('File uploaded!');
     }
   } catch (err) {
