6f5412660bb340d7667f8fb9905119247af41832
bhumilsarvaiya <bhumilsarvaiya@gmail.com>
2018-02-16 12:06:22 +0530

    scrollbar width change

--- indexer/src/App.vue
+++ indexer/src/App.vue
@@ -23,7 +23,7 @@ export default {
   margin-top: 20px;
 }
 ::-webkit-scrollbar {
-    width: 10px;
+    width: 8px;
 }
 /* Track */
 ::-webkit-scrollbar-track {
@@ -33,12 +33,13 @@ export default {
 
 /* Handle */
 ::-webkit-scrollbar-thumb {
-    background: #888888;
+    background: #666666;
     border-radius: 10px;
 }
 
 /* Handle on hover */
 ::-webkit-scrollbar-thumb:hover {
-    background: #333333;
+    background: #555555;
 }
+
 </style>
